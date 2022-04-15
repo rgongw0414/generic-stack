a213a05a7a3d093767214cc9ddefc7613db0d177
rgongw0414 <a1055547@mail.nuk.edu.tw>
2022-04-16 02:15:06 +0800

    update test.c

--- test.c
+++ test.c
@@ -8,18 +8,23 @@ typedef struct car{
 }car;
 
 int main(int argc, char* argv[]){
-	stack* myStack = stackCreate(ELSE, 3, sizeof(car));
+    int capacity = 3;
+    printf("initializing a stack with capacity: %d, element_type: car\n", capacity);
+	stack* myStack = stackCreate(ELSE, capacity, sizeof(car));
 	car* myCar = malloc(sizeof(car));
+
 	myCar->name = "toyota";
 	myCar->price = 100;
 	push(myStack, myCar);
     car* top = (car*)get_top(myStack);
+    printf("\n%s pushed, top element: \n\t", myCar->name);
     printf("name: %s, price: %d\n", top->name, top->price);
 
 	myCar->name = "suzuki";
 	myCar->price = 300;
 	push(myStack, myCar);
     top = (car*)get_top(myStack);
+    printf("\n%s pushed, top element: \n\t", myCar->name);
     printf("name: %s, price: %d\n", top->name, top->price);
 
 
@@ -27,22 +32,28 @@ int main(int argc, char* argv[]){
 	myCar->price = 500;
 	push(myStack, myCar);
     top = (car*)get_top(myStack);
+    printf("\n%s pushed, top element: \n\t", myCar->name);
     printf("name: %s, price: %d\n", top->name, top->price);
 
 	myCar->name = "benz";
 	myCar->price = 1000;
 	push(myStack, myCar);
     top = (car*)get_top(myStack);
+    printf("try to push %s, but stack is full \n\ttop element: \n\t", myCar->name);
     printf("name: %s, price: %d\n", top->name, top->price);
 
 	pop(myStack);
+    printf("\npoped, top element: \n\t");
+    printf("name: %s, price: %d\n", top->name, top->price);
+
 	myCar->name = "benz";
 	myCar->price = 1000;
 	push(myStack, myCar);
     top = (car*)get_top(myStack);
+    printf("\n%s pushed, top element: \n\t", myCar->name);
     printf("name: %s, price: %d\n", top->name, top->price);
 
-	printf("stack:\n");
+	printf("-\nstack:\n");
 	for (int i = 0; i <= myStack->top; i++){
 		printf("name: %s, price: %d\n", ((car*)myStack->data)[i].name, ((car*)myStack->data)[i].price);
     }
